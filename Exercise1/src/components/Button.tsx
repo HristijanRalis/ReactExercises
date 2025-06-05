704b23ecfd29eee9c893b338d61fca775b3bce8f
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-06-05 23:54:25 +0200

    Added folder components and two files Button and Heading

---
+++ Exercise1/src/components/Button.tsx
@@ -0,0 +1,10 @@
+import { Component } from 'react'
+
+ class Button extends Component {
+  render() {
+    return (
+      <div>Button</div>
+    )
+  }
+}
+export default Button;
---
+++ Exercise1/src/components/Heading.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'react'
+
+export default class Heading extends Component {
+  render() {
+    return (
+      <div>Heading</div>
+    )
+  }
+}
