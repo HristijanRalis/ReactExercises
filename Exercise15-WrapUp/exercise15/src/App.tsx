60ae0b6fc0c5d40852c45e85bcac5e7746e93c55
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-08-06 18:22:13 +0200

    Done with exercise15

--- Exercise15-WrapUp/exercise15/src/App.tsx
+++ Exercise15-WrapUp/exercise15/src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./Pages/HomePage/HomePage";
 import { DetailPage } from "./Pages/DetailPage/DetailPage";
 
 function App() {
- 
-
   return (
     <>
       <h1>Countries</h1>
