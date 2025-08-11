666e24b7f172c92709afde49a9e8bd384d6622e8
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-08-11 10:55:48 +0200

    Added firebase into project

---
+++ Exercise16-Firebase1/exercise16/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDp7oKBDxd_jd7WPPIYYlNJMl4aEyPRO6w",
+  authDomain: "countries-d348d.firebaseapp.com",
+  projectId: "countries-d348d",
+  storageBucket: "countries-d348d.firebasestorage.app",
+  messagingSenderId: "767285644416",
+  appId: "1:767285644416:web:d852f51d67f911dcd36a65",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
